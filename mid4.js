515f5c461d67c177c7fda1f97cce75f9901c631f
Yoon, DeuckGi <isz03072@gmail.com>
2020-05-04 10:00:49 +0900

    세번째

---
+++ mid4.js
@@ -0,0 +1,17 @@
+function sum(...a) {
+    let result = 0;
+    for (let i = 0; i < a.length; ++i)
+    result += a[i];
+    return result;
+    }
+
+function test_sum(f){
+    let result= sum
+    for(let j=0; i<6; j++){
+        arr[i]=(Math.floor(Math.random()*(10-1))+1);
+     }
+}
+
+for(let k=0; k<6; k++){
+    console.log(test_sum)
+}
